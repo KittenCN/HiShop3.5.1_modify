feb1aae30285658c61811e43e30d3b12f41c4c7a
Peng Lv <owdely@vip.163.com>
2018-09-18 18:35:32 +0800

    temp upload

--- public/Hidistro.UI.SaleSystem.CodeBehind/Hidistro/UI/SaleSystem/CodeBehind/VSubmmitOrder.cs
+++ public/Hidistro.UI.SaleSystem.CodeBehind/Hidistro/UI/SaleSystem/CodeBehind/VSubmmitOrder.cs
@@ -38,6 +38,7 @@
         private Literal litShipTo;
         private Literal litShowMes;
         private Literal litUseMembersPointShow;
+        private Literal litOrderStutas;
         private HtmlInputHidden MembersPointMoney;
         private string productSku;
         private HtmlInputHidden regionId;
@@ -67,6 +68,8 @@
             this.MembersPointMoney = (HtmlInputHidden) this.FindControl("MembersPointMoney");
             this.regionId = (HtmlInputHidden) this.FindControl("regionId");
             this.litAddAddress = (Literal) this.FindControl("litAddAddress");
+            this.litOrderStutas = (Literal)this.FindControl("litOrderStutas");
+
             IList<ShippingAddressInfo> shippingAddresses = MemberProcessor.GetShippingAddresses();
             this.rptAddress.DataSource = from item in shippingAddresses
                 orderby item.IsDefault
